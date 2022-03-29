263677b9b8278f47e0d974d3d6ebfe03e7501c24
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2022-03-28 20:28:36 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#ifndef COMMIT
+#define COMMIT [Unknown Commit]
+#endif
+#define STRINGIFY(x) #x
+#define COMMIT_STR STRINGIFY(COMMIT)
+
 int main()
 {
     std::cout << "Hello, CI/CD!\n";
+    std::cout << "I was compiled with commit " << COMMIT_STR << "!\n";
     return 0;
 }
